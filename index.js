313e6d9d2a574bc7cc87d2773fbc590d2a87b975
Akhilesh1902 <akhil1922002@gmail.com>
2021-11-15 16:53:58 +0530

    connected the netlify submission

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const navList = document.querySelector(".nav--list");
 const navBtn = document.querySelector(".nav--btn");
+const navLink = document.querySelectorAll(".nav li ");
 
 navBtn.addEventListener("click", () => {
   navList.classList.toggle("change");
+  navLink.forEach((link, index) => {
+    if (link.style.animation) link.style.animation = "";
+    else {
+      link.style.animation = `navAnim 0.5s ease ${index / 7 + 1}s`;
+    }
+    if (link.style.opacity) console.log("here");
+    console.log(index / 7);
+  });
 });
+console.log(navLink);
